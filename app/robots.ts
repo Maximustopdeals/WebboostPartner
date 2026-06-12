fa59ae8f2ff250b08c2abafcf3782e42b04e60c5
Maximustopdeals <76074846+Maximustopdeals@users.noreply.github.com>
2026-06-12 12:42:29 +0200

    Refactor robots function to use MetadataRoute type

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,12 @@
-export default function robots() {
+import type { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: "https://webboostpartner.nl/sitemap.xml",
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [],
+    },
+    sitemap: 'https://webboostpartner.nl/sitemap.xml',
   };
 }
